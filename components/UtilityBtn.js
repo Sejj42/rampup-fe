b11832920be4e2efd13ce0f5cd9510a7c374f7fa
Sejj42 <omer162@gmail.com>
2022-02-18 19:47:15 +0200

    functioning delete pod utility button (w/o UI refresh)

--- components/UtilityBtn.js
+++ components/UtilityBtn.js
@@ -1,20 +1,36 @@
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import styles from "@/styles/UtilityBtn.module.css";
 import Image from "next/image";
 import { API_URL } from "../config";
+import { useRouter } from "next/router";
 const UtilityBtn = ({ utility, imgsrc, alttext, bgcolor, podid }) => {
-  const deletePod = async (podid) => {
-    const res = await fetch(`${API_URL}/pods/${podid}`, { method: "DELETE" });
+  const router = useRouter();
+
+  const deletePod = async () => {
+    if (confirm("Are you sure?")) {
+      const res = await fetch(`${API_URL}/pods/${podid}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.push("/manage-pods");
+      }
+    }
   };
 
   const handleButtonClick = () => {
     console.log(utility, "button has been pressed");
-    // if ((utility = "Delete")) {
-    //   deletePod(podid);
-    // }
+    if (utility === "Delete") {
+      deletePod();
+    }
   };
   return (
     <button
-      className={[styles.btnContainer]}
+      className={styles.btnContainer}
       style={{ backgroundColor: bgcolor }}
       onClick={handleButtonClick}
     >
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import styles from "@/styles/Home.module.css";
 import BigRoundLinkBtn from "@/components/BigRoundLinkBtn";
+import PageTitle from "@/components/PageTitle";
 
 export default function Home() {
   return (
     <>
+      <div className={styles.homeTitle}>
+        <PageTitle
+          bigTitle="Good to see you!"
+          subTitle="What are we up to today?"
+        />
+      </div>
       <div className={styles.container}>
-        {/* <div>
-          <h1>Welcome home, partner!</h1>
-        </div> */}
         <div className={styles.btnsContainer}>
           <BigRoundLinkBtn text="Pods" linkTo="/manage-pods" />
           <BigRoundLinkBtn text="Orders" linkTo="/orders" />
