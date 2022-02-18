5a7051d7dadfd25fd826140c0df3cd5687eb9944
Sejj42 <omer162@gmail.com>
2022-02-18 15:22:12 +0200

    logout clause of sidebar fixed ui wise

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,50 +6,65 @@ import sidebarStyles from "@/styles/Sidebar.module.css";
 const Sidebar = () => {
   return (
     <div className={sidebarStyles.sideNav}>
-      <section className={sidebarStyles.logoBar}>
-        <Link href="/" passHref>
+      <div className={sidebarStyles.bigDivTop}>
+        <section className={sidebarStyles.logoBar}>
+          <Link href="/" passHref>
+            <Image
+              src="/fitto_logo_bar.png"
+              alt="fitto_logo_bar"
+              style={{ width: "230px", height: "80px" }}
+            />
+          </Link>
+        </section>
+        <section className={sidebarStyles.logoAndBrandName}>
+          <span className={sidebarStyles.fittoImgLogo}>
+            <Image
+              src="https://i.ibb.co/7tC01BY/fitto.png"
+              alt="fitto-rounded-logo"
+              className="fitto-rounded-logo"
+              style={{ width: "70px", height: "70px" }}
+            />
+          </span>
+          <span>fitto</span>
+        </section>
+        <section className={sidebarStyles.navItemSection}>
+          <section className={sidebarStyles.navItem}>
+            <Link href="/manage-pods" passHref>
+              <Image
+                src="pod_icon.png"
+                alt="icon"
+                style={{ width: "35px", height: "35px", marginRight: "12px" }}
+              />
+            </Link>
+            <Link href="/manage-pods" passHref>
+              <span>Manage Pods</span>
+            </Link>
+          </section>
+          <section className={sidebarStyles.navItem}>
+            <Link href="/orders" passHref>
+              <Image
+                src="bag_icon.png"
+                alt="bag_icon"
+                style={{ width: "35px", height: "35px", marginRight: "12px" }}
+              />
+            </Link>
+            <Link href="/orders" passHref>
+              <span>Orders</span>
+            </Link>
+          </section>
+        </section>
+      </div>
+      <div className={sidebarStyles.logOutClause}>
+        <div className={sidebarStyles.hr}></div>
+        <div className={sidebarStyles.logOutIconAndBtn}>
           <Image
-            src="/fitto_logo_bar.png"
-            alt="fitto_logo_bar"
-            style={{ width: "230px", height: "80px" }}
-          />
-        </Link>
-      </section>
-      <section className={sidebarStyles.logoAndBrandName}>
-        <span className={sidebarStyles.fittoImgLogo}>
-          <Image
-            src="https://i.ibb.co/7tC01BY/fitto.png"
-            alt="fitto-rounded-logo"
-            className="fitto-rounded-logo"
-            style={{ width: "70px", height: "70px" }}
-          />
-        </span>
-        <span>fitto</span>
-      </section>
-      <section className={sidebarStyles.navItem}>
-        <Link href="/manage-pods" passHref>
-          <Image
-            src="pod_icon.png"
-            alt="icon"
+            src="logout_icon.png"
+            alt="logout_icon"
             style={{ width: "35px", height: "35px", marginRight: "12px" }}
           />
-        </Link>
-        <Link href="/manage-pods" passHref>
-          <span>Manage Pods</span>
-        </Link>
-      </section>
-      <section className={sidebarStyles.navItem}>
-        <Link href="/orders" passHref>
-          <Image
-            src="bag_icon.png"
-            alt="icon"
-            style={{ width: "35px", height: "35px", marginRight: "12px" }}
-          />
-        </Link>
-        <Link href="/orders" passHref>
-          <span>Orders</span>
-        </Link>
-      </section>
+          <button className={sidebarStyles.logOutBtn}>Log Out</button>
+        </div>
+      </div>
     </div>
   );
 };
